01f17fd2501a2d25aec759bf0fc84a7598d4a6ce
Victor Oliveira Morais de Vasconcelos <147342141+Victor-OMVasconcelos@users.noreply.github.com>
2024-09-04 00:42:16 -0300

    Add files via upload

---
+++ variableScope/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //local = declared inside a method, visible only to that method
+        // constructor counts as method
+
+        //global = declared outside a method, but within a class, visible to all parts of a class
+        //you can make it global by adding parameters and arguments or do what i did in DiceRoller class
+
+        DiceRoller diceRoller = new DiceRoller();
+    }
+}
