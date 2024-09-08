57fdbc74ed2b78c457ef99fbedfda9ae898dc1b6
Victor Oliveira Morais de Vasconcelos <147342141+Victor-OMVasconcelos@users.noreply.github.com>
2024-09-07 23:17:52 -0300

    Add files via upload

---
+++ toString/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+         car cars = new car();
+
+       // System.out.println(cars.toString());
+        System.out.println(cars);
+    }
+}
